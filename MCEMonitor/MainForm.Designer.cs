bab13c77ffcdd6296897e33d99f3c900732d0c1f
Skypichat-kodi <skypichat@hotmail.fr>
2026-07-04 19:46:30 +0200

    Changement de version vers 1.4

--- MCEMonitor/MainForm.Designer.cs
+++ MCEMonitor/MainForm.Designer.cs
@@ -933,7 +933,7 @@ namespace MCEMonitor
                 "----------------------------------------\n" +
                 "LICENCE MIT\n" +
                 "----------------------------------------\n\n" +
-            @"MCEMonitor (Version 1.1)
+            @"MCEMonitor (Version 1.4)
             Outil de supervision et d'automatisation pour Media Server (KODI).
 
             MIT License
--- MCEMonitorInstaller.iss
+++ MCEMonitorInstaller.iss
@@ -5,7 +5,7 @@
 
 [Setup]
 AppName=MCEMonitor
-AppVersion=1.3
+AppVersion=1.4
 DefaultDirName={autopf}\MCEMonitor
 DefaultGroupName=MCEMonitor
 OutputDir=Installer
